3da22d413d09b9098ba9db9c084c4bfbd66239be
matthew-howe <mattehowe@gmail.com>
2019-02-13 21:19:10 -0500

    update

--- codewars/6-kyu/longest-palindrome.js
+++ codewars/6-kyu/longest-palindrome.js
@@ -8,8 +8,8 @@
 // If the length of the input string is 0, the return value must be 0.
 
 // Example:
-// "a" -> 1 
-// "aab" -> 2  
+// "a" -> 1
+// "aab" -> 2
 // "abcde" -> 1
 // "zzbaabcd" -> 4
 // "" -> 0
@@ -20,19 +20,19 @@
 //MANACHER'S ALGORITHM
 function manachers(s) {
   var d1 = [],
-      d2 = [],
-      n = s.length,
-      l = 0,
-      r = -1;
+    d2 = [],
+    n = s.length,
+    l = 0,
+    r = -1;
   for (var i = 0; i < n; ++i) {
-    var k = (i > r ? 0 : Math.min(d1[l + r - i], r - i + 1));
+    var k = i > r ? 0 : Math.min(d1[l + r - i], r - i + 1);
     while (i + k < n && i - k >= 0 && s[i + k] === s[i - k]) {
-      ++k;   
+      ++k;
     }
     d1[i] = k--;
-    if(i + k > r) {
+    if (i + k > r) {
       l = i - k;
-      r = i + k;    
+      r = i + k;
     }
   }
   l = 0;
@@ -40,21 +40,20 @@ function manachers(s) {
   for (var i = 0; i < n; ++i) {
     var k = (i > r ? 0 : Math.min(d2[l + r - i + 1], r - i + 1)) + 1;
     while (i + k - 1 < n && i - k >= 0 && s[i + k - 1] === s[i - k]) {
-      ++k;   
+      ++k;
     }
     d2[i] = --k;
-    if(i + k - 1 > r) {
+    if (i + k - 1 > r) {
       l = i - k;
-      r = i + k - 1;    
+      r = i + k - 1;
     }
   }
 
   return [d1, d2];
 }
 
-
 // CODEWARS ALGORITHM
-function codeWars(s){
+function codeWars(s) {
   // check if the input is empty
   if (!s) return 0;
   // set a variable for the length that decrements down to 0
@@ -65,7 +64,14 @@ function codeWars(s){
       // set a variable for each substring
       var check = s.substr(i, c);
       // if the substring is the same forwards as backwards it is the longest substring
-      if (check === check.split("").reverse().join("")) return c;
+      if (
+        check ===
+        check
+          .split('')
+          .reverse()
+          .join('')
+      )
+        return c;
     }
   }
 }
@@ -73,38 +79,41 @@ function codeWars(s){
 // W3 ALGORITHM
 function w3helper(str1) {
   // create a helper function that reverses the characters in a string.
-  var rev = str1.split("").reverse().join("");
+  var rev = str1
+    .split('')
+    .reverse()
+    .join('');
   return str1 == rev;
 }
-  
-function w3(str1){
+
+function w3(str1) {
   // create variable to store longest word length
   var max_length = 0,
-  // create variable to store the longest word 
-  maxp = '';
+    // create variable to store the longest word
+    maxp = '';
   // create a variable that loops from 0 to the input length
-  for(var i=0; i < str1.length; i++) {
+  for (var i = 0; i < str1.length; i++) {
     // create a variable for the substrings created by looping the start index up to the input length
     var subs = str1.substr(i, str1.length);
-      // create a variable that decrements from the length of the first variable down to 0
-      for(var j=subs.length; j>=0; j--) {
-        // create a variable that represents the substring of the first substring and the end of the input
-        // looping smaller and smaller
-        var sub_subs_str = subs.substr(0, j);
-        // if this newly created substring is 1 or less characters, skip that iteration
-        if (sub_subs_str.length <= 1) continue;
-        // check if the inner substring is a palindrome
-        if (w3helper(sub_subs_str)) {
-          // check if the inner substring is the longest found
-          if (sub_subs_str.length > max_length) {
-            // if its the longest set it to that variable
-            max_length = sub_subs_str.length;
-            // set the string to the longest palindrome variable
-            maxp = sub_subs_str;
-          }
+    // create a variable that decrements from the length of the first variable down to 0
+    for (var j = subs.length; j >= 0; j--) {
+      // create a variable that represents the substring of the first substring and the end of the input
+      // looping smaller and smaller
+      var sub_subs_str = subs.substr(0, j);
+      // if this newly created substring is 1 or less characters, skip that iteration
+      if (sub_subs_str.length <= 1) continue;
+      // check if the inner substring is a palindrome
+      if (w3helper(sub_subs_str)) {
+        // check if the inner substring is the longest found
+        if (sub_subs_str.length > max_length) {
+          // if its the longest set it to that variable
+          max_length = sub_subs_str.length;
+          // set the string to the longest palindrome variable
+          maxp = sub_subs_str;
         }
       }
     }
+  }
   // return the longest palindrome found
   return maxp;
 }
