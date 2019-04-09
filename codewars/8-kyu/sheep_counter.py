ee3f561c3455192ec8439dc20a815bdfe4268f98
matthew-howe <mattehowe@gmail.com>
2019-04-09 13:53:21 -0400

    sheep counter

---
+++ codewars/8-kyu/sheep_counter.py
@@ -0,0 +1,10 @@
+# If you can't sleep, just count sheep!!
+
+# Task:
+# Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+def count_sheep(n):
+    output = ''
+    for i in range(1, n+1):
+        output+=str(i) + " sheep..."
+    return output
