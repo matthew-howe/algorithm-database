26d646a34c273a5c92d2414588b48905df840b35
matthew-howe <mattehowe@gmail.com>
2019-07-30 19:27:35 -0400

    Fixes error in stock prices

--- cake/stock_prices.cpp
+++ cake/stock_prices.cpp
@@ -15,36 +15,28 @@ using namespace std;
 
 int getMaxProfit(const vector<int>& stockPrices)
 {
-	if (stockPrices.size() < 2) {
-		throw invalid_argument("Getting a profit requires at least 2 prices");
-	}
-	
-	// initialize minPrice to the first price
-	// initialize maxProfit to the second price minus the first
-	int minPrice = stockPrices[0];
-	int maxProfit = stockPrices[1] - stockPrices[0];
+    if (stockPrices.size() < 2) {
+        throw invalid_argument("Getting a profit requires at least 2 prices");
+    }
 
-	// starting at the second index loop through the vector
-	for (size_t i = 1; stockPrices.size(); ++i) {
-		int currentPrice = stockPrices[i];
+    int minPrice = stockPrices[0];
+    int maxProfit = stockPrices[1] - stockPrices[0];
 
-		// check what our profit would be if we bought at the min
-		// and sold at the current
-		int potentialProfit = currentPrice - minPrice;
+    for (size_t i = 1; i < stockPrices.size(); ++i) {
+        int currentPrice = stockPrices[i];
 
-		// update the maxProfit if we can do better
-		maxProfit = max(maxProfit, potentialProfit);
+        int potentialProfit = currentPrice - minPrice;
 
-		// update minPrice so it's always
-		// the lowest price we've seen so far
-		minPrice = min(minPrice, currentPrice);
-	}
-	return maxProfit;
+        maxProfit = max(maxProfit, potentialProfit);
+
+        minPrice = min(minPrice, currentPrice);
+    }
+
+    return maxProfit;
 }
 
 int main()
 {
-	vector<int> v = {10, 5, 5, 8, 11, 9};
-	cout << v << endl;
-	/* cout << getMaxProfit(v) << endl; */
+	vector<int> v {10, 5, 5, 8, 11, 9};
+	cout << getMaxProfit(v) << endl;
 }
