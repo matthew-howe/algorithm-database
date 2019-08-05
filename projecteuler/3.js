4d02878f7987257624d55af101a6807f32a9613e
matthew-howe <mattehowe@gmail.com>
2019-08-05 16:55:22 -0400

    Adds problem 3 in javascript

---
+++ projecteuler/3.js
@@ -0,0 +1,17 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the number 600851475143 ?
+
+var originalTarget = 600851475143;
+var target = originalTarget;
+var i = 2;
+while(i<target) {
+	while(target%i === 0) {
+		(function(newtarget) {
+			console.log(target + " can be divided by " + i + " which gives us " + newtarget);
+			target = newtarget;
+		})(target / i)
+	}
+	i++;
+}
+console.log("it seems like " + target + " is the biggest prime factor for " + originalTarget);
