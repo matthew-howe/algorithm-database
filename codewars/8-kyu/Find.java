11f84b2405fac897c5e4a582ce5d178351dd3e16
matthew-howe <mattehowe@gmail.com>
2019-08-21 16:23:41 -0400

    Adds you only need one in java

---
+++ codewars/8-kyu/Find.java
@@ -0,0 +1,12 @@
+public class Find {
+
+    public static boolean check(Object[] a, Object x) {
+        
+        for (int i = 0; i < a.length; i++) {
+          if (a[i] == x) return true; 
+        }
+        
+        return false;
+    }
+
+}
