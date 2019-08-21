4f0c01ae86bd2d24bd12b186d5013659e42a8c8d
matthew-howe <mattehowe@gmail.com>
2019-08-21 15:18:19 -0400

    Adds hacker rank sql queries

--- hackerrank/sql/basic_select.sql
+++ hackerrank/sql/basic_select.sql
@@ -24,3 +24,46 @@ FROM CITY;
 SELECT *
 FROM CITY
 WHERE ID = 1661;
+
+-- # Problem: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+-- # Problem: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+SELECT CITY, STATE FROM STATION;
+
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0;
+
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
+
+-- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+SELECT * 
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY), CITY)
+WHERE ROWNUM = 1
+UNION
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY) DESC, CITY)
+WHERE ROWNUM = 1;
