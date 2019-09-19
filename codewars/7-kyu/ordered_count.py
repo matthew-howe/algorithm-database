c5be46e0a1ecfaeac6d1fcbce3521781d3ae82ab
matthew-howe <mattehowe@gmail.com>
2019-09-19 11:55:26 -0400

    Adds prompt

--- codewars/7-kyu/ordered_count.py
+++ codewars/7-kyu/ordered_count.py
@@ -1,8 +1,8 @@
-Count the number of occurrences of each character and return it as a list of tuples in order of appearance.
-
-Example:
-
-ordered_count("abracadabra") == [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)]
+# Count the number of occurrences of each character and return it as a list of tuples in order of appearance.
+#
+# Example:
+#
+# ordered_count("abracadabra") == [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)]
 
 def ordered_count(str):
     letter_list = []
