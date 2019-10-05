13ea95a8d5c39801929342191a3e5b3a15a35c79
matthew-howe <mattehowe@gmail.com>
2019-10-05 19:42:54 -0400

    fixes 3 product in js

--- cake/3product.js
+++ cake/3product.js
@@ -1,73 +1,53 @@
-    // sort the array
-    // initialize a variable for highest product
-    // initialize an array for the product of two combos
-    // 
-    // at each iteration keep track of
-        // highest product of 3
-        // highest product of 2
-        // highest
-        // lowest product of 2
-        // lowest
+// sort the array
+// initialize a variable for highest product
+// initialize an array for the product of two combos
+//
+// at each iteration keep track of
+// highest product of 3
+// highest product of 2
+// highest
+// lowest product of 2
+// lowest
 
 function highestProductOf3(arrayOfInts) {
+  if (arrayOfInts.length < 3)
+    throw new Error('Less than 3 integers in the input');
 
-    if (arrayOfInts.length < 3) throw new Error("Less than 3 integers in the input");
+  arrayOfInts = arrayOfInts.sort((a, b) => a - b);
 
-    arrayOfInts = arrayOfInts.sort((a, b) => a - b);
+  let highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+  let highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+  let highest =
+    arrayOfInts[0] > arrayOfInts[1] ? arrayOfInts[0] : arrayOfInts[1];
+  let lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+  let lowest =
+    arrayOfInts[0] < arrayOfInts[1] ? arrayOfInts[0] : arrayOfInts[1];
 
-    let highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
-    let highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
-    let highest = arrayOfInts[0] > arrayOfInts[1] ? arrayOfInts[0] :
-        arrayOfInts[1];
-    let lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
-    let lowest = arrayOfInts[0] < arrayOfInts[1] ? arrayOfInts[0] :
-        arrayOfInts[1];
+  for (let i = 2; i < arrayOfInts.length; i++) {
+    const num = arrayOfInts[i];
 
-    for (let i = 2; i < arrayOfInts.length; i++) {
-        const num = arrayOfInts[i]; 
-
-        if ((num * highestProductOf2) > highestProductOf3) {
-            highestProductOf3 = num * highestProductOf2;
-        }
-
-        if ((num * lowestProductOf2) > highestProductOf3) {
-            highestProductOf3 = num * lowestProductOf2;
-        }
-
-        if ((num * highest) > highestProductOf2) {
-            highestProductOf2 = num * highest;
-        }
-
-        if ((num * lowest) < lowestProductOf2) {
-            lowestProductOf2 = num * lowest;
-        }
-
-        if (num > highest) highest = num;
-        if (num < lowest) lowest = num;
+    if (num * highestProductOf2 > highestProductOf3) {
+      highestProductOf3 = num * highestProductOf2;
     }
 
-  
-    
+    if (num * lowestProductOf2 > highestProductOf3) {
+      highestProductOf3 = num * lowestProductOf2;
+    }
+
+    if (num * highest > highestProductOf2) {
+      highestProductOf2 = num * highest;
+    }
+
+    if (num * lowest < lowestProductOf2) {
+      lowestProductOf2 = num * lowest;
+    }
+
+    if (num > highest) highest = num;
+    if (num < lowest) lowest = num;
+  }
 
   return highestProductOf3;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Tests
 
@@ -97,22 +77,22 @@ expected = -6;
 assertEqual(actual, expected, desc);
 
 desc = 'error with empty array';
-const emptyArray = () => (highestProductOf3([]));
+const emptyArray = () => highestProductOf3([]);
 assertThrowsError(emptyArray, desc);
 
 desc = 'error with one number';
-const oneNumber = () => (highestProductOf3([1]));
+const oneNumber = () => highestProductOf3([1]);
 assertThrowsError(oneNumber, desc);
 
 desc = 'error with two numbers';
-const twoNumber = () => (highestProductOf3([1, 1]));
+const twoNumber = () => highestProductOf3([1, 1]);
 assertThrowsError(twoNumber, desc);
 
 function assertEqual(a, b, desc) {
   if (a === b) {
     console.log(`${desc} ... PASS`);
   } else {
-    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
   }
 }
 
