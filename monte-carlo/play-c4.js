44179f78baaf5e1ff608c46c6c3d57240fa7a9f4
matthew-howe <mattehowe@gmail.com>
2020-05-16 14:19:33 -0400

    monte carlo connect 4 game states

---
+++ monte-carlo/play-c4.js
@@ -0,0 +1,12 @@
+class Play_C4 {
+  constructor(row, col) {
+    this.row = row
+    this.col = col
+  }
+
+  hash() {
+    return this.row.toString() + "," + this.col.toString()
+  }
+}
+
+module.exports = Play_C4;
