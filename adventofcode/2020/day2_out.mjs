b42f21d088badeff43e87d1bb5e27b86aa005ddc
matthew-howe <mattehowe@gmail.com>
2021-08-31 02:22:54 -0400

    adds day2

---
+++ adventofcode/2020/day2_out.mjs
@@ -0,0 +1,48 @@
+import x from './day2_in.mjs';
+
+let y = x.split('\n');
+let z = 0;
+
+// Part 1
+// const vld = (p) => {
+//     let vals = p.split(' ');
+//     let minmax = vals[0].split('-');
+//     let min = minmax[0];
+//     let max = minmax[1];
+//     let char = vals[1][0];
+//     let str = vals[2];
+//     let count = 0
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === char) count++;
+//     }
+//     return count >= min && count <= max;
+// }
+
+// for (let i = 0; i < y.length; i++) {
+//     let l = y[i];
+//     let res = vld(l);
+//     if (res) z++;
+// }
+
+// part 2
+const vld = (p) => {
+    let vals = p.split(' ');
+    let minmax = vals[0].split('-');
+    let min = minmax[0];
+    let max = minmax[1];
+    let char = vals[1][0];
+    let str = vals[2];
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) count++;
+    }
+    return count >= min && count <= max;
+}
+
+for (let i = 0; i < y.length; i++) {
+    let l = y[i];
+    let res = vld(l);
+    if (res) z++;
+}
+
+console.log(z);
